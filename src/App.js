53db38220f51ae53fe3eb6a7faead6d716678ad6
sageeth007 <sageeth007@gmail.com>
2024-06-28 07:21:09 +0100

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import logo from './logo.svg';
+//auto gen content below
+/* import logo from './logo.svg';
 import './App.css';
 
 function App() {
@@ -17,6 +18,31 @@ function App() {
         >
           Learn React
         </a>
+      </header>
+    </div>
+  );
+}
+
+export default App; */
+
+////////////////////////////////////////////////////////////
+///copied from chatGPT
+import React, { useEffect, useState } from 'react';
+import './App.css';
+
+function App() {
+  const [message, setMessage] = useState('');
+
+  useEffect(() => {
+    fetch(`${process.env.REACT_APP_API_URL}/`)
+      .then((response) => response.text())
+      .then((message) => setMessage(message));
+  }, []);
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>{message}</p>
       </header>
     </div>
   );
